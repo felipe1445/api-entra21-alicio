74c56f962af366d478e26600483eeba006ed0a46
felipe1445 <fel_dav@hotmail.com>
2019-09-12 22:04:13 -0300

    novas classes

--- api-chamados/src/main/webapp/bootstrap/js/app.js
+++ api-chamados/src/main/webapp/bootstrap/js/app.js
@@ -3,9 +3,7 @@ var appEntra21 = angular.module("appEntra21", ['ngRoute']);
 appEntra21.config(function($routeProvider, $locationProvider){
     
 	$routeProvider
-    .when("/chamados",{templateUrl:'view/chamado-insert.html', controller:'chamadoController'})
-    .when("/chamados-listar",{templateUrl:'view/chamado.html', controller:'chamadoController'})
-    .when("/chamados-editar/:id",{templateUrl:'view/chamado-edit.html', controller:'chamadoDetalheController'})
+    .when("/chamados",{templateUrl:'view/chamado.html', controller:'chamadoController'})
     .when("/chamados/:id",{templateUrl:'view/chamado-detalhe.html', controller:'chamadoDetalheController'})
     .when("/usuario",{templateUrl:'view/usuario.html', controller:'usuarioController'})
     .otherwise({redirectTo:'/'});
--- api-chamados/src/main/webapp/js/controller/chamado-detalhe-controller.js
+++ api-chamados/src/main/webapp/js/controller/chamado-detalhe-controller.js
@@ -2,10 +2,12 @@ appEntra21.controller("chamadoDetalheController", function($scope, $http,
 		$routeParams) {
 
 	$scope.chamadoDetalhe = {};
+	
 	var url = 'http://localhost:8080/api-entra21/rest/chamados/';
 
 		$http.get(url + $routeParams.id).then(function(response) {
 			$scope.chamadoDetalhe = response.data;
+			$scope.chamado = response.data
 			console.log('success - chamadoDetalheController');
 			console.log(response.data);
 			console.log(response.status);
@@ -15,4 +17,20 @@ appEntra21.controller("chamadoDetalheController", function($scope, $http,
 			console.log(response.data);
 			console.log(response.status);
 		});
+		
+		$scope.updateChamado = function() {			
+			metodo = 'PUT';		
+
+			$http({
+				method : metodo,
+				url : url,
+				data : $scope.chamadoDetalhe
+			}).then(function(response) {
+				alert("salvo");
+			}, function(response) {
+				console.log('error do salvar');
+				console.log(response.data);
+				console.log(response.status);
+			});
+		};
 });
